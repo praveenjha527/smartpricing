4d7445a4c7d924a22871b896e799ddec65b00cc2
Shaumyadeep Chaudhuri <shaumya@gmail.com>
2015-08-27 15:54:41 +0530

    Initial Models

--- src/smart_price/models.py
+++ src/smart_price/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Price(models.Model):
+    product_id = models.IntegerField()
+    brand_id = models.IntegerField()
+    category_id = models.IntegerField()
+    region_id = models.IntegerField()
+
+
+    @property
+    def minprice(self):
+        pass
+
+    @property
+    def maxprice(self):
+        pass
+
+    @property
+    def sellerprices(self):
+        pass
