9592094636258daae0983ee9e52526e7c0b0c16e
shemnt.raj38@gmail.com <shemnt.raj38@gmail.com>
2015-08-27 20:39:33 +0530

    dynamic price refactoring

--- src/zopzam/settings.py
+++ src/zopzam/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'smart_price'
+    'smart_price',
+    'django_rq',
 )
 
 MIDDLEWARE_CLASSES = (
