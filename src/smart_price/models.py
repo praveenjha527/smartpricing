dce8d563283d35b0aab4adbe17fc08db70986a81
Shaumyadeep Chaudhuri <shaumya@gmail.com>
2015-08-27 20:24:33 +0530

    Model Changes

--- src/smart_price/models.py
+++ src/smart_price/models.py
@@ -1,35 +1,71 @@
 import requests
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 
 PRICES_HOST = "http://45.56.112.85"
 
 CRAWLED_HOST = "http://128.199.238.212/"
 CRAWLED_URN = "product/minprice/{product_id}/"
 
+
+class VariationRule(models.Model):
+    rule_name = models.CharField(max_length=255)
+    rule_operator = models.CharField(max_length=1)
+
+
+class VariationFactor(models.Model):
+    factor_value = models.FloatField()
+    factor_types = models.ForeignKey(VariationRule)
+    rule_weight = models.IntegerField()
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    factor_target = GenericForeignKey('content_type', 'object_id')
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+    variation_factors = GenericRelation(VariationFactor, related_query_name='variation_factors')
+
+
+class Category(models.Model):
+    category_id = models.IntegerField()
+    variation_factors = GenericRelation(VariationFactor, related_query_name='variation_factors')
+
+
+class Region(models.Model):
+    region_id = models.IntegerField()
+    variation_factors = GenericRelation(VariationFactor, related_query_name='variation_factors')
+
+
 class Product(models.Model):
     product_id = models.IntegerField()
-    brand = models.CharField(max_length=255)
-    category_id = models.IntegerField()
-    region_id = models.IntegerField()
+    brand = models.ForeignKey(Brand)
+    category = models.ForeignKey(Category)
+    region = models.ForeignKey(Region)
+    variation_factors = GenericRelation(VariationFactor, related_query_name='variation_factors')
+
+    class Meta:
+        unique_together = (('product_id', 'brand', 'category', 'region'),)
 
 
     @property
-    def minprice(self):
+    def min_online_price(self):
         request_url = "{0}/{1}".format(
             CRAWLED_HOST, CRAWLED_URN.format(product_id=self.product_id)
         )
         resp = requests.get(request_url)
         resp_json = resp.json()
-        return resp_json['min_price']
+        return resp_json['min_price'] or 0
 
     @property
-    def maxprice(self):
+    def max_online_price(self):
         request_url = "{0}/{1}".format(
             CRAWLED_HOST, CRAWLED_URN.format(product_id=self.product_id)
         )
         resp = requests.get(request_url)
         resp_json = resp.json()
-        return resp_json['max_price']
+        return resp_json['max_price'] or 0
 
     @property
     def seller_prices(self):
@@ -54,6 +90,8 @@ class Product(models.Model):
                 })
         except TypeError as exc:
             pass
-        return store_prices
+        return store_prices or []
+
+
 
 
--- src/smart_price/serializer.py
+++ src/smart_price/serializer.py
@@ -6,7 +6,9 @@ class ProductSerializer(serializers.ModelSerializer):
     seller_prices = serializers.ListField(read_only=True)
     minprice = serializers.FloatField(read_only=True)
     maxprice = serializers.FloatField(read_only=True)
-
+    brand = serializers.SlugRelatedField(slug_field='name', read_only=True)
+    category = serializers.SlugRelatedField(slug_field='category_id', read_only=True)
+    region = serializers.SlugRelatedField(slug_field='region_id', read_only=True)
 
     class Meta:
         model = Product
