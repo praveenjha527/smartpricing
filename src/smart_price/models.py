e26e613aff09cb9574788f0f989bd615923c183a
Shaumyadeep Chaudhuri <shaumya@gmail.com>
2015-08-27 19:07:44 +0530

    More Changes

--- src/smart_price/models.py
+++ src/smart_price/models.py
@@ -3,21 +3,33 @@ from django.db import models
 
 PRICES_HOST = "http://45.56.112.85"
 
+CRAWLED_HOST = "http://128.199.238.212/"
+CRAWLED_URN = "product/minprice/{product_id}/"
 
 class Product(models.Model):
     product_id = models.IntegerField()
-    brand_id = models.IntegerField()
+    brand = models.CharField(max_length=255)
     category_id = models.IntegerField()
     region_id = models.IntegerField()
 
 
     @property
     def minprice(self):
-        pass
+        request_url = "{0}/{1}".format(
+            CRAWLED_HOST, CRAWLED_URN.format(product_id=self.product_id)
+        )
+        resp = requests.get(request_url)
+        resp_json = resp.json()
+        return resp_json['min_price']
 
     @property
     def maxprice(self):
-        pass
+        request_url = "{0}/{1}".format(
+            CRAWLED_HOST, CRAWLED_URN.format(product_id=self.product_id)
+        )
+        resp = requests.get(request_url)
+        resp_json = resp.json()
+        return resp_json['max_price']
 
     @property
     def seller_prices(self):
@@ -32,11 +44,16 @@ class Product(models.Model):
         }
         response = requests.get("{0}/{1}".format(PRICES_HOST, urn), headers=headers)
         resp_json = response.json()
-        store_prices =[]
-        for store in resp_json.get('stores'):
-            store_prices.append({
-                'store_id': store['id'],
-                'price':  store['price']
-            })
-
+        store_prices = []
+        try:
+            for store in resp_json.get('stores'):
+                store_prices.append({
+                    'store_id': store['id'],
+                    'price':  store['price'],
+                    'is_lfr': True if store['chain_id'] else False
+                })
+        except TypeError as exc:
+            pass
         return store_prices
+
+
--- src/smart_price/serializer.py
+++ src/smart_price/serializer.py
@@ -4,5 +4,9 @@ from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     seller_prices = serializers.ListField(read_only=True)
+    minprice = serializers.FloatField(read_only=True)
+    maxprice = serializers.FloatField(read_only=True)
+
+
     class Meta:
         model = Product
