d354a7168b330c48f483e391e74476084e867df6
Shaumyadeep Chaudhuri <shaumya@gmail.com>
2015-08-28 01:53:36 +0530

    So much code

--- src/smart_price/tasks.py
+++ src/smart_price/tasks.py
@@ -11,75 +11,8 @@ from django.core.mail import send_mail
 from .models import Product
 from schedule import schedule_once
 
-# def dynamic_price_calculation():
-#     """
-#     This will dynamically calculate the prices of products on the various parameters and offsets
-#     :return:
-#     """
-#     resultant_prices={}
-#     all_products=Product.objects.all()
-#     for product in all_products:
-#         product_id=product.product_id
-#         brand_id=product.brand_id
-#         category_id=product.category_id
-#         region_id=product.region_id
-#         seller_prices=product.seller_prices
-#         if seller_prices:
-#             lsp=min([x['price'] for x in seller_prices if not x['is_lfr']])
-#             lsp_stores = filter(lambda x : x==lsp and not x['is_lfr'], seller_prices)
-#             #todo get from dashboard or auto generated V
-#             lsp_store_factor=10
-#             V=lsp_store_factor
-#             V_amount=(V*lsp)/100
-#             lsp_near_stores=filter(lambda x : x['price'] <=lsp+V_amount, seller_prices)
-#
-#             min_online_price=product.min_online_price
-#
-#             zopper_selling_prices=lsp
-#
-#             if min_online_price !=0:
-#                 # this will be dependent  on other factors like trasfer price,etc
-#                 zopper_selling_prices=min_online_price-1
-#
-#             if zopper_selling_prices > lsp:
-#                 zopper_selling_prices = lsp
-#
-#
-#             # lfr stores
-#             lfr_stores=filter(lambda x: x['is_lfr'],seller_prices)
-#             if lfr_stores:
-#                 lfr_lsp=min([x['price'] for x in seller_prices if x['is_lfr']])
-#                 lfr_lsp_stores= filter(lambda x : x['price']==lfr_lsp,lfr_stores)
-#                 lfr_lsp_near_stores=filter(lambda x : x['price'] < lfr_lsp,lfr_stores)
-#
-#             for item in seller_prices:
-#                 if item['is_lfr']:
-#                     if item in lfr_lsp_stores:
-#                         resultant_prices[item['store_id']]=zopper_selling_prices
-#
-#                     elif item in lfr_lsp_near_stores:
-#                         resultant_prices[item['store_id']]=item['price']-abs(zopper_selling_prices-lsp)
-#                     else:
-#                         resultant_prices[item['store_id']]=item['price']
-#                 else:
-#                     if item in lsp_near_stores:
-#                         resultant_prices[item['store_id']]=item['price']-abs(zopper_selling_prices-lfr_lsp)
-#                     else:
-#                         resultant_prices[item['store_id']]=item['price']
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# schedule_once(dynamic_price_calculation,interval=60*30)
-#
+def dynamic_price_calculation():
+    RuleProcessor().process_all()
 
 
 class RuleProcessor(object):
