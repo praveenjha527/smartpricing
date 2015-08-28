2dc10178e7e379631525819c42f1ece45b1a5f03
Praveen Jha <praveenrocks.jha370@gmail.com>
2015-08-28 07:34:35 +0530

    SQL data

--- src/smart_price/tests.py
+++ src/smart_price/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class APItest(TestCase):
+    def api_test(self):
+        url='http://localhost:8000/suggested_prices/'
+        import  requests
+        filter_params={
+            ""
+        }
+        data=requests.get(url,filter_params)
